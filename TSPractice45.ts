a41444df9eadfb98f6b8cfb7f4f042927f419615
Hammad Ahmad <139546876+hammad5421@users.noreply.github.com>
2023-10-21 11:48:30 +0500

    Add files via upload

---
+++ TSPractice45.ts
@@ -0,0 +1,417 @@
+// let sentence = "Our virtual card, which comes with your account when you sign up, will still be available for free for online payments. We understand that many of you don’t use your physical card frequently, and wanted to make sure that you can still enjoy the rest of our services with ease";
+
+// let subs = sentence.substring(0,100);
+// console.log(subs);
+
+// -----------------------------------------------------------------------------------------
+
+// let str:string = " free for online payments. We understand that many of you don’t use your physical card frequently, and wanted to make sure t"
+
+// let str1:string = " Our virtual card, which comes with your account";
+
+// let comparestr:boolean = str === str1;
+
+// console.log(comparestr);
+// -----------------------------------------------------------------------------------------
+// let productName:string = "Pr"
+// let productPrice:number = 220;
+
+
+// console.log(`The product name is ${productName} and the price is ${productPrice}`);
+
+// -----------------------------------------------------------------------------------------
+
+// function isEven(a:number):boolean {
+//     if(a%2==0){
+//     return true
+//     }
+//     else{
+//         return false
+//     }
+// }
+
+// console.log(isEven(21))
+
+// -----------------------------------------------------------------------------------------
+
+// function isDivisibleBy(a:number):boolean{
+//     return (a%4===0 || a%8===0)
+    
+// }
+
+
+// console.log(isDivisibleBy(16));
+
+
+
+// -----------------------------------------------------------------------------------------
+
+// --------------------Bigint-----------
+
+// let bignumber:bigint = 216265165132165n;
+// let anotherbignumber:bigint = 21626516565265216132165n;
+
+// console.log(bignumber+anotherbignumber);
+// console.log(anotherbignumber-bignumber);
+// console.log(anotherbignumber*bignumber);
+// console.log(anotherbignumber/bignumber);
+
+
+// console.log("Hammad you will not give this time if you will give up its mean you will give up your life")
+
+// let personName2:string= "Hammad";
+// console.log( `"Helo=lo `  , personName2 , ` would you like to learn some python today"`)
+
+// let newstr1:string = personName2.toUpperCase();
+// console.log(newstr1)
+// let newstr2:string = personName2.toLowerCase()
+// console.log(newstr2)
+// let newstr3:string = personName2.case
+// console.log(newstr2)
+
+
+//  Practice question # 1
+// let myName = "hammad "
+// console.log(`Hello ${myName}, would you like
+// to learn some Python today?`);
+
+// //  Practice question # 2
+// let myName = "hammad "
+// console.log(myName.toUpperCase())
+// console.log(myName.toLowerCase())
+
+// //  Practice question # 3
+
+// let authour= "Albert Einstein";
+// let qoute=`“A person who never made a mistake never tried anything
+// new.”`
+
+// console.log(`${authour} once said ${qoute}`);
+
+// //  Practice question # 4
+
+// let myname = " \t\n hammad \t\n"
+// console.log(myname);
+
+
+// //  Practice question # 5
+
+// console.log( 3+5);
+// console.log(13-5);
+// console.log(2*4);
+// console.log(16/2);
+
+// //  Practice question # 6
+
+// let myfavnumber= 7
+// console.log(`My favourite number is ${myfavnumber}`);
+
+// //  Practice question # 7
+
+// let myArray= ["Farhan" , "Faisal", "Husnain", "hamad"]
+// console.log( `Greetings! ${myArray[1]} Have a nice day`);
+// console.log( `Greetings! ${myArray[2]} You are looking good`);
+// console.log( `Greetings! ${myArray[3]} Have a nice meeting you have`);
+
+
+// //  Practice question # 8
+
+// let favTransports=["Car", "bike", "train"]
+// console.log(`I love to ride on ${favTransports[0]}`);
+// console.log(`I love to ride on ${favTransports[1]}`);
+// console.log(`I love to ride on ${favTransports[2]}`);
+
+// // //  Practice question # 9
+//  let favcharacter = ["Shahid Afridi", " Fakhir " , "hashir"]
+//  console.log(`Hey ${favcharacter[0]} I would like to invite you on the Event `);
+//  console.log(`Hey ${favcharacter[1]} I would like to invite you on the Event `);
+//  console.log(`Hey ${favcharacter[2]} I would like to invite you on the Event `);
+ 
+// // //  Practice question # 10
+
+//  let favcharacter = ["Shahid Afridi", " Fakhir " , "hashir"]
+//  console.log(`  \t\n Hey ${favcharacter[0]} I would like to invite you on the Event\t\n  `);
+//  console.log(` \t\n  Hey ${favcharacter[1]} I would like to invite you on the Event \t\n  `);
+//  console.log(`\t\n  ${favcharacter[2]} is unable to atten the Event \t\n `);
+
+//  favcharacter.pop();
+//  favcharacter.push("Hammad AHmad")
+//  console.log(`\t\n So, in replacement we invited Mr. ${favcharacter[2]}\t\n `);
+
+
+// // //  Practice question # 11
+
+// let favcharacter = ["Shahid Afridi", " Fakhir " , "hashir"]
+
+// console.log(`  \t\n Hey ${favcharacter[0]} I would like to invite you on the Event\t\n  `);
+// console.log(` \t\n  Hey ${favcharacter[1]} I would like to invite you on the Event \t\n  `);
+// console.log(`So we have some more guest on Dinner table. Stay tuned for them`);
+// favcharacter.unshift("Malik")
+// favcharacter.splice(2, 0, "Hammad");
+// favcharacter.push("end")
+// let new1 = favcharacter
+// console.log(new1);
+// console.log(`  \t\n Hey ${favcharacter[0]} I would like to invite you on the Event\t\n  `);
+// console.log(` \t\n  Hey ${favcharacter[5]} I would like to invite you on the Event \t\n  `);
+// console.log(` \t\n  Hey ${favcharacter[2]} I would like to invite you on the Event \t\n  `);
+
+// // //  Practice question # 12
+
+// let favcharacter = ["Shahid Afridi", " Fakhir " , "hashir"]
+// favcharacter.unshift("Malik")
+// favcharacter.splice(2, 0, "Hammad");
+// favcharacter.push("end")
+// console.log(`Dear ${favcharacter[5]} Unfortunately, we can't have you with us on the dinner becasuse of less space.`);
+// favcharacter.pop()
+
+// console.log(`Dear ${favcharacter[4]} Unfortunately, we can't have you with us on the dinner becasuse of less space.`);
+// favcharacter.pop()
+
+// console.log(`Dear ${favcharacter[3]} Unfortunately, we can't have you with us on the dinner becasuse of less space.`);
+// favcharacter.pop()
+
+// console.log(`Dear ${favcharacter[2]} Unfortunately, we can't have you with us on the dinner becasuse of less space.`);
+// favcharacter.pop()
+
+
+// console.log(`Dear ${favcharacter[1]} and  ${favcharacter[0]} Don't worry , you are still invited.`);
+
+// favcharacter.pop()
+// favcharacter.pop()
+// console.log(favcharacter);
+
+
+// // //  Practice question # 13
+
+// let placesToVisit1 = [
+//     "Tokyo, Japan",
+//     "Machu Picchu, Peru",
+//     "Santorini, Greece",
+//     "Banff, Canada",
+//     "Cape Town, South Africa"
+//   ];
+  
+
+//     console.log(placesToVisit1);
+
+//     placesToVisit1.sort();
+//     // console.log(placesToVisit1);
+    
+//     placesToVisit1.reverse()
+//     console.log(placesToVisit1);
+
+//     placesToVisit1.reverse()
+//     console.log(placesToVisit1);
+
+
+// Original array
+// const originalArray: string[] = ['apple','cherry', 'date', 'banana', 'grape'];
+
+// console.log(originalArray);
+// originalArray.sort();
+// console.log(originalArray);
+// originalArray.reverse();
+// console.log(originalArray);
+
+
+// // Create a copy of the original array
+// const reversedArray: string[] = originalArray.slice();
+
+// // Sort the copy in reverse alphabetical order
+// reversedArray.sort((a, b) => b.localeCompare(a));
+
+// // Print the reversed array
+// for (const item of reversedArray) {
+//     console.log(item);
+// }
+
+    
+// // //  Practice question # 14
+
+
+  
+// let favcharacter = ["Shahid Afridi", " Fakhir " , "hashir"]
+// console.log(`There are ${favcharacter.length} persons are invited`);
+
+
+
+// // //  Practice question # 15
+
+// let anArray = ["ravi" , "chenab" , "satluj" ,"SIndh"]
+// console.log(anArray);
+
+// console.log('List of RRIvers in pakisatn:');
+
+// anArray.forEach(element => {
+//     console.log(element);
+    
+// });
+
+// Creating an array of countries
+// const countries: string[] = ['USA', 'Canada', 'Brazil', 'India', 'China', 'Australia', 'Russia', 'South Africa', 'Germany', 'Japan'];
+
+// // Printing the list of countries
+// console.log('List of countries:');
+// for (const country of countries) {
+//     console.log(country);
+// }
+
+// // //  Practice question # 16
+
+// let obj1= {
+//     river1: "ravi",
+//     river2: "chenab",
+//     river3: "satluj",
+//     river4: "sindh"
+// }
+
+// console.log(obj1[river1]);
+
+// // //  Practice question # 17
+// let car = "honda"
+// let isCar = true
+
+// console.log("Is car == 'subaru'? I predict True.")
+//  console.log(car === "subaru") 
+
+    //  console.log(car=="honda");
+    //  console.log(car=='honda');
+    //  console.log(car === "12") ;
+    //  console.log(10>12) ;
+    //  console.log(10<12) ;
+    //  console.log(10<12  || 9 === 9) ;
+    //  console.log(10>12  && 9 === 9) ;
+
+
+    //  Practice question # 18
+
+//     let myArray = [1, 2, 3, 4, 5];
+// let itemToFind = 6;
+
+// console.log(`Is ${itemToFind} in myArray? I predict True.`);
+// console.log(myArray.includes(itemToFind));
+
+// console.log(!myArray.includes(itemToFind));
+
+// if (alien_color==="green"){
+//     console.log(`player just earned 5 points`);   
+// }
+// else if (alien_color!=="green"){
+//     console.log(`player just earned 10 points`);   
+// }
+// else if (alien_color==="red"){
+//     console.log(`player just earned 15 points`);   
+// }
+// else if (alien_color==="Yellow"){
+//     console.log(`player just earned 5 points`);   
+// }
+// else {
+//     console.log("dafa Hojao je be rang rehna h te ");
+    
+// }
+
+//  Practice question # 19
+
+// let alien_color  = "red"
+
+// if (alien_color==="green"){
+//     console.log(`player just earned 5 points`);   
+// }
+
+// else if (alien_color==="red"){
+//     console.log(`player just earned 15 points`);   
+// }
+// else if (alien_color==="Yellow"){
+//     console.log(`player just earned 5 points`);   
+// }
+// else {
+//     console.log("dafa Hojao je be rang rehna h te ");
+    
+// }
+
+//  Practice question # 20
+
+// let person_age = 45;
+
+// if ( person_age >= 20 || person_age<65 )
+// {
+//     console.log("the person is older ")
+// }
+
+// let favoriteFruits = ['apple', 'banana', 'anaar', 'Pineapple']
+// if (favoriteFruits.includes('banana')) {
+//     console.log('Banana is one of my favorite fruits!');
+//   }
+  
+//   if (favoriteFruits.includes('apple')) {
+//     console.log('I love apples!');
+//   }
+  
+//   if (favoriteFruits.includes('orange')) {
+//     console.log('I don\'t like oranges.');
+//   }
+//   if (favoriteFruits.includes('Pineapple')) {
+//     console.log('I love Pineapple!');
+//   }
+  
+//   if (favoriteFruits.includes('Leechi')) {
+//     console.log('I don\'t like Leechi.');
+//   }
+  
+
+
+// for (let index = 0; index < usernames.length; index++) {
+//   if (usernames[index]==='admin'){
+//     console.log(`Hello admin, would you like to see a status report? `);
+//   }
+//   else{
+//     console.log(`Hello ${usernames[index]}, thank you for logging in again. `);
+    
+//   }
+// }
+
+//  Practice question # 21
+
+// let usernames : any[] =  ['apple', 'banana', 'strawberry', 'kiwi', 'mango'];
+// if(usernames.length<1)
+// {
+//   console.log("we need to find some user");
+  
+// }
+// usernames.forEach(element => {
+ 
+//   if (element==='admin'){
+//     console.log(`Hello admin, would you like to see a status report? `);
+//   }
+//   else{
+//     console.log(`Hello ${element}, thank you for logging in again. `);
+    
+//   }
+// });
+// usernames.pop();
+// usernames.pop();
+// usernames.pop();
+// usernames.pop();
+// usernames.pop();
+// if(usernames.length<1)
+// {
+//   console.log("we need to find some user");
+  
+// }
+
+
+//  Practice question # 22
+
+let current_Users = ["Hammad","Hammad1","Hammad2","Hammad3","Hammad4"]
+let new_Users = ["hammad4","Hammad7","Hammad8","Hammad9","Hammad4"]
+
+
+// Loop through new_users list
+for (let newUser of new_Users) {
+  // Check if the new username is in the list of existing usernames
+  if (current_Users.includes(newUser)) {
+    console.log(`Username "${newUser}" is already in use. Please choose a different username.`);
+  } else {
+    console.log(`Username "${newUser}" is available.`);
+  }
+}
